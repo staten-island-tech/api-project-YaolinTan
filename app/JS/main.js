79b40c6feb81befeb2b238d657ed395092e7bfbb
YaolinTan <yaolint@nycstudents.net>
2024-11-20 11:25:41 -0500

    E

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,2 +1,20 @@
 import "../CSS/style.css";
 console.log("Hello World");
+
+async function getData(URL) {
+  //fetch returns a promise
+  try {
+    const response = await fetch(URL);
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry");
+  }
+}
+getData("https://api-nba-v1.p.rapidapi.com/seasons/");
