c9ee45d4072c92926bdba27f668606ffc7250268
YaolinTan <yaolint@nycstudents.net>
2024-11-20 12:30:32 -0500

    e

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,7 +1,20 @@
 import "../CSS/style.css";
 console.log("Hello World");
 
+const URL = "https://stats.nba.com/stats/allstarballotpredictor";
+
 async function getData(URL) {
+  console.log("Fetching data...");
+  try {
+    const response = await fetch(URL);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
+
+/*async function getData(URL) {
   //fetch returns a promise
   try {
     const response = await fetch(URL);
@@ -17,4 +30,4 @@ async function getData(URL) {
     alert("sorry");
   }
 }
-getData("https://api-nba-v1.p.rapidapi.com/seasons/");
+getData();*/
