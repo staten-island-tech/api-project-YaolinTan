7e008db4646bd34b175bb96a2c91c419f9040b69
YaolinTan <yaolint@nycstudents.net>
2024-11-21 12:28:23 -0500

    e

--- app/main.js
+++ app/main.js
@@ -1,1 +1,37 @@
 import "./style.css";
+
+async function getData() {
+  try {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        "https://rapidapi.com/api-sports/api/api-nba"
+      )}`
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
+
+async function getData() {
+  //fetch returns a promise
+  try {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        "https://rapidapi.com/api-sports/api/api-nba"
+      )}`
+    );
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry");
+  }
+}
+getData();
