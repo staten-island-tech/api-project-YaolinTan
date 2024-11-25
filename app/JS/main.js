1aba612e2d866ae6a4c0e46dbec856015194b907
YaolinTan <yaolint@nycstudents.net>
2024-11-25 12:32:31 -0500

    e

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,20 +1,31 @@
 import "../CSS/style.css";
 console.log("Hello World");
 
-const url =
-  "https://basketball-data.p.rapidapi.com/tournament/info?tournamentId=89";
-const options = {
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "b547fab545msh1e011958ac061d1p1962dcjsn97b955f29017",
-    "x-rapidapi-host": "basketball-data.p.rapidapi.com",
-  },
+const fetchPlayer = async (id) => {
+  const url = `https://nba-api-free-data.p.rapidapi.com/nba-player-listing/v1/data?id=${id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "b547fab545msh1e011958ac061d1p1962dcjsn97b955f29017",
+      "x-rapidapi-host": "nba-api-free-data.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(`Data for player ID ${id}:`, data);
+    } else {
+      console.error(
+        `Failed to fetch data for ID ${id}. Status: ${response.status}`
+      );
+    }
+  } catch (error) {
+    console.error(`Error fetching data for ID ${id}:`, error);
+  }
 };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+for (let i = 1; i <= 30; i++) {
+  fetchPlayer(i);
 }
